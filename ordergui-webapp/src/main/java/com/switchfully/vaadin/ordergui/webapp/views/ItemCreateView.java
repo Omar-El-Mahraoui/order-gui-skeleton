0202b3a43d3cd60ca7f5d990cb662f1a37b2de6f
El Mahraoui Omar (100) <omar.elmahraoui@cmc.be>
2018-04-26 15:41:18 +0200

    added changing views

--- ordergui-webapp/src/main/java/com/switchfully/vaadin/ordergui/webapp/views/ItemCreateView.java
+++ ordergui-webapp/src/main/java/com/switchfully/vaadin/ordergui/webapp/views/ItemCreateView.java
@@ -5,8 +5,13 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
+
+// copied and adapted code from https://github.com/stevendecock/vaadinbooking
+// and switchfully repositories
 
 public class ItemCreateView extends CustomComponent implements View {
 
@@ -16,12 +21,54 @@ public class ItemCreateView extends CustomComponent implements View {
     @Autowired
     public ItemCreateView(ItemResource itemResource) {
         mainLayout = new VerticalLayout();
-        mainLayout.addComponent(new Label("Test"));
+        mainLayout.setMargin(true);
         setCompositionRoot(mainLayout);
+
+        init();
+    }
+
+    private void init() {
+        mainLayout.removeAllComponents();
+
+        addHeader();
+        addNameForm();
+        addDescriptionForm();
+        addPriceAndAmountOfStockForm();
+        addCreateAndCancelButtons();
+    }
+
+    private void addHeader() {
+        Label labelHeader = new Label("New Item");
+        labelHeader.setStyleName(ValoTheme.LABEL_H1);
+        mainLayout.addComponent(labelHeader);
+    }
+
+    private void addNameForm() {
+        Label labelName = new Label("Name");
+        TextField textFieldName = new TextField();
+        textFieldName.setInputPrompt("Fill in the item name...");
+        textFieldName.setSizeFull();
+        mainLayout.addComponents(labelName, textFieldName);
+    }
+
+    private void addDescriptionForm() {
+        Label labelDescription = new Label("Description");
+        TextField textFieldDescription = new TextField();
+        textFieldDescription.setInputPrompt("Fill in the item description...");
+        textFieldDescription.setSizeFull();
+        mainLayout.addComponents(labelDescription, textFieldDescription);
+    }
+
+    private void addPriceAndAmountOfStockForm() {
+
+    }
+
+    private void addCreateAndCancelButtons() {
+
     }
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
-
+        init();
     }
 }
--- ordergui-webapp/src/main/java/com/switchfully/vaadin/ordergui/webapp/views/ItemOverviewView.java
+++ ordergui-webapp/src/main/java/com/switchfully/vaadin/ordergui/webapp/views/ItemOverviewView.java
@@ -1,22 +1,25 @@
 package com.switchfully.vaadin.ordergui.webapp.views;
 
-// copied and adapted code from https://github.com/stevendecock/vaadinbooking
-// and switchfully repositories
-
 import com.switchfully.vaadin.ordergui.interfaces.items.Item;
 import com.switchfully.vaadin.ordergui.interfaces.items.ItemResource;
+import com.switchfully.vaadin.ordergui.webapp.OrderGUI;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.data.util.filter.SimpleStringFilter;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
+// copied and adapted code from https://github.com/stevendecock/vaadinbooking
+// and switchfully repositories
+
 public class ItemOverviewView extends com.vaadin.ui.CustomComponent implements View{
 
     private Grid grid;
     private VerticalLayout mainLayout;
     private ItemResource itemResource;
+    private BeanItemContainer<Item> container;
 
     @Autowired
     public ItemOverviewView(ItemResource itemResource) {
@@ -50,11 +53,26 @@ public class ItemOverviewView extends com.vaadin.ui.CustomComponent implements V
         TextField filterField = new TextField();
         filterField.setInputPrompt("Filter by name");
         filterField.setSizeFull();
+        filterField.addTextChangeListener(event -> {
+            container.removeAllContainerFilters();
+            container.addContainerFilter(new SimpleStringFilter("name", event.getText()
+                    , true, false));
 
-        Button buttonFilter = new Button("FIlter");
+        });
+
+        Button buttonFilter = new Button("Filter");
         buttonFilter.setStyleName(ValoTheme.BUTTON_PRIMARY);
+        buttonFilter.addClickListener(event -> {
+            container.removeAllContainerFilters();
+            container.addContainerFilter("name", filterField.getValue(), true, false);
+        });
 
-        header.addComponents(labelItems, filterField, buttonFilter);
+        Button buttonNewItem = new Button("New Item");
+        buttonNewItem.setStyleName(ValoTheme.BUTTON_FRIENDLY);
+        buttonNewItem.addClickListener(event -> getUI().getNavigator().navigateTo(OrderGUI.CREATE_ITEM));
+
+
+        header.addComponents(labelItems, filterField, buttonFilter, buttonNewItem);
         mainLayout.addComponent(header);
     }
 
@@ -64,7 +82,7 @@ public class ItemOverviewView extends com.vaadin.ui.CustomComponent implements V
 //                        mainLayout.addComponent(
 //                                new HorizontalLayout(
 //                                        new Label("--> " + item.name + " €" + item.price))));
-        BeanItemContainer<Item> container = new BeanItemContainer<>(Item.class, itemResource.getItems());
+        container = new BeanItemContainer<>(Item.class, itemResource.getItems());
         grid.setColumns("name", "description", "price", "amountOfStock");
         grid.setContainerDataSource(container);
         mainLayout.addComponent(grid);
